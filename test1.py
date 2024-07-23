335080480c0bbdf169f408f83ac4af6001ea72fa
CreatorGodX <godofcreation980316@gmail.com>
2024-07-23 15:28:02 +0800

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,14 @@
+#test
+weather = input ('tell me if the weather today is good or not.(anser me good or not good)')
+if weather == 'not good':
+    print ('Oh, so bad!')
+if weather == 'good':
+	print ('yeeees!')
+if weather != 'good' 'notgood':
+	print ('your answer is wrong! Please answer again' )
+	
+
+
+
+
+
